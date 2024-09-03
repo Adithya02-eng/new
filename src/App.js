8ff35658649c5a962398ff828822a6b6e076f7b7
Adithya02-eng <lalithadithyasarabu@gmail.com>
2024-09-03 19:08:33 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Head from './Header.js'
+
+function App() {
+  return (
+    <div className="App">
+      <Head/>;
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <Header><h1>Hi Friends ros</h1></Header>
+  )
+}
+
+export default Header
